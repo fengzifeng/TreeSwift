227cfe2e95536e2e41a0ee8cd7aa1e7534294232
StoryMatrix01 <>
2024-06-05 17:26:21 +0800

    提交1

--- TreeSwift/class/tree/ArticleListViewController.swift
+++ TreeSwift/class/tree/ArticleListViewController.swift
@@ -62,6 +62,7 @@ class ArticleListViewController: BaseViewController {
         tableView.mj_footer = MJRefreshAutoNormalFooter(refreshingBlock: requestMoreData)
         view.addSubview(tableView)
         requestData()
+        
     }
     
 }
