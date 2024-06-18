a5dba6db5af2ba3e9547e6f79ac2110c5c5d9d85
StoryMatrix01 <>
2024-06-18 15:05:07 +0800

    测试2dev_1_2

--- TreeSwift/class/home/CycleCell.swift
+++ TreeSwift/class/home/CycleCell.swift
@@ -15,7 +15,6 @@ class CycleCell: UICollectionViewCell {
         super.init(frame: frame)
         self.createSubviews(frame: frame)
     }
-
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
